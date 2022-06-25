186e0f2a271a399bd66aec192f865afec709d9f8
Dharmil Shah <dharmilshah476@gmail.com>
2022-06-26 00:24:21 +0530

    Used Props ExpenseItem.js Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+
+  const expenseDetails = [
+    {
+      title: "Car Insurance",
+      date: "2020-01-01",
+      amount: 100
+    },
+    {
+      title: "Budget Trip",
+      date: "2020-01-01",
+      amount: 200
+    },
+    {
+      title: "Rent",
+      date: "2020-01-01",
+      amount: 300
+    },
+    {
+      title: "Food",
+      date: "2020-01-01",
+      amount: 400
+    },
+  ]
+
   return (
     <>
-    <div className="App">
-    <ExpenseItem />
-    </div>
+      <div className="App">
+        <ExpenseItem title={expenseDetails[0].title} date={expenseDetails[0].date} amount={expenseDetails[0].amount} />
+
+        <ExpenseItem title={expenseDetails[1].title} date={expenseDetails[1].date} amount={expenseDetails[1].amount} />
+
+        <ExpenseItem title={expenseDetails[2].title} date={expenseDetails[2].date} amount={expenseDetails[2].amount} />
+
+        {/* <ExpenseItem title={expenseDetails.title} date={expenseDetails.date} amount={expenseDetails.amount}/> */}
+      </div>
     </>
   );
 }
