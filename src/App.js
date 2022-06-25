54644c56aa69eb9f7ded3361f71f15e1ce161da6
Dharmil Shah <dharmilshah476@gmail.com>
2022-06-25 23:01:06 +0530

    Created One Custom Component ExpenseItem.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
+    <>
     <div className="App">
       <h2>Hello There</h2>
       <p>Welcome To React Practice Session</p>
+    <ExpenseItem />
     </div>
+    </>
   );
 }
 
