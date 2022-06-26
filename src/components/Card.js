57f0fbbd2c80cc9f304268f4ef32cad3b2fdf14a
Dharmil Shah <dharmilshah476@gmail.com>
2022-06-26 11:34:26 +0530

    Added Card.js and Card.css in order to reduce the duplication of both JS code and CSS code. Card is the custom component.

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Card.css'
+
+function Card(props) {
+    const classes = 'card ' + props.className;
+  return (
+    <div className={classes}>{props.children}</div> // This props.children is by default available in the Card component. whether we mention it specially or not, it will be available.
+  )
+}
+
+export default Card
---
+++ src/components/Expenses.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+
+function Expenses(props) {
+    return (
+        <Card className="expenses">
+            <ExpenseItem title={props.expenseDetails[0].title} date={props.expenseDetails[0].date} amount={props.expenseDetails[0].amount} />
+
+            <ExpenseItem title={props.expenseDetails[1].title} date={props.expenseDetails[1].date} amount={props.expenseDetails[1].amount} />
+
+            <ExpenseItem title={props.expenseDetails[2].title} date={props.expenseDetails[2].date} amount={props.expenseDetails[2].amount} />
+        </Card>
+    )
+}
+
+export default Expenses
