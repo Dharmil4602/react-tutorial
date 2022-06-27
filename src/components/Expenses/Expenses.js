7f84fedcc14e5a6e963fb1d9aa6c4fca097b8777
Dharmil Shah <dharmilshah476@gmail.com>
2022-06-27 23:30:08 +0530

    Added the special method to add the data dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,21 +5,32 @@ import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
 
 function Expenses(props) {
-    const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-    return (
-        <Card className="expenses">
-            <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            <ExpenseItem title={props.expenseDetails[0].title} date={props.expenseDetails[0].date} amount={props.expenseDetails[0].amount} />
+  return (
+    <Card className="expenses">
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-            <ExpenseItem title={props.expenseDetails[1].title} date={props.expenseDetails[1].date} amount={props.expenseDetails[1].amount} />
+      {/* We are writing below code to render the entered title and all other details dynamically */}
+      {props.expenseDetails.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />))}
 
-            <ExpenseItem title={props.expenseDetails[2].title} date={props.expenseDetails[2].date} amount={props.expenseDetails[2].amount} />
-        </Card>
-    )
+        {/* As we will be adding the data dynamically, we can remove the below code. */}
+      {/* <ExpenseItem title={props.expenseDetails[0].title} date={props.expenseDetails[0].date} amount={props.expenseDetails[0].amount} />
+
+      <ExpenseItem title={props.expenseDetails[1].title} date={props.expenseDetails[1].date} amount={props.expenseDetails[1].amount} />
+
+      <ExpenseItem title={props.expenseDetails[2].title} date={props.expenseDetails[2].date} amount={props.expenseDetails[2].amount} /> */}
+      
+    </Card>
+  )
 }
 
 export default Expenses
